4ad98208a90a05cb4bb85eb2f3527768e62e528b
Duffey_kay <kayodesunday29@gmail.com>
2022-06-15 10:36:59 +0100

    added new sass files, modified existing files, renamed files

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import CategoryItem from '../category-item/category-item.component';
+import './directory.style.scss'
+
+const Directory = ({categories}) => {
+  return (
+    <div className='directory-container'>
+      {
+        categories.map((category)=>{
+          return(
+            <CategoryItem key={category.id} categoryProp = {category} />
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default Directory
